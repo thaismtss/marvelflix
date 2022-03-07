3e2640b6e143c7259ec6d06351f032288b44c5d9
Thais Martins <thaismartins1999@gmail.com>
2022-03-07 19:43:53 -0300

    create Logo component

---
+++ src/componets/logo/Logo.jsx
@@ -0,0 +1,12 @@
+import "./Logo.css"
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="Logo">
+            <span>MARVELFLIX</span>
+        </div>
+    )
+}
+
+export default Logo
